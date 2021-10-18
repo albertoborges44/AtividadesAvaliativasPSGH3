67fb36e70fa18ffe05900267886f1c893616a5a3
albertoborges44 <90792748+albertoborges44@users.noreply.github.com>
2021-10-18 13:46:31 -0400

    Add files via upload

---
+++ Encontro23-JPA/src/main/java/principal/Programa.java
@@ -0,0 +1,34 @@
+package principal;
+
+import dominio.*;
+import repositorio.*;
+import servico.*;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		CategoriaRepositorio repo = new CategoriaRepositorio("exemplo-jpa");
+		CategoriaServico servico = new CategoriaServico(repo);
+		SubCategoriaRepositorio repoSub = new SubCategoriaRepositorio("exemplo-jpa");
+		SubCategoriaServico servicoSub = new SubCategoriaServico(repoSub);
+		ProdutoRepositorio repoPro = new ProdutoRepositorio("exemplo-jpa");
+		ProdutoServico servicoPro = new ProdutoServico(repoPro);
+		
+		servicoPro.inserir();
+		
+		for (Categoria item : servico.listar()) {
+			System.out.println(item);
+		}
+		
+		for (SubCategoria item : servicoSub.listar()) {
+			System.out.println(item);
+		}
+		
+		for (Produto item : servicoPro.listar()) {
+			System.out.println(item);
+		}
+		
+		repo.dispose();
+
+	}
+}
